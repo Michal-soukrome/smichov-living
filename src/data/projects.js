74a721055c8e1eb14fd8dd851c06e45abb1dff62
Michal Jirák <majklmajklson@gmail.com>
2026-06-28 18:01:54 +0200

    dalsi z mnoha

---
+++ src/data/projects.js
@@ -0,0 +1,25 @@
+// src/data/projects.js
+
+export const projects = [
+  {
+    tag: "Byt 3+KK",
+    title: "Byt na Smíchově",
+    desc: "Kompletní rekonstrukce bytu včetně návrhu interiéru a výroby nábytku na míru.",
+    href: "/reference/byt-na-smichove",
+    image: "/images/projects/homepage.webp",
+  },
+  {
+    tag: "Rodinný dům",
+    title: "Rodinný dům – Praha západ",
+    desc: "Kompletní realizace interiéru rodinného domu v nadčasovém přírodním stylu.",
+    href: "/reference/rodinny-dum-praha-zapad",
+    image: "/images/projects/sluzby.webp",
+  },
+  {
+    tag: "Byt 4+KK",
+    title: "Moderní byt v centru Prahy",
+    desc: "Návrh dispozice, realizace na klíč a kompletní vybavení interiéru prémiovými materiály.",
+    href: "/reference/moderni-byt-centrum-prahy",
+    image: "/images/projects/showroom.webp",
+  },
+];
